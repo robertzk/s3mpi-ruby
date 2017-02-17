7a055c545c21df53a15c33afca13f2f5e4272074
David Feldman <david.feldman@avant.com>
2017-02-17 16:16:22 -0600

    just parse the json once with quirks_mode turned on

--- lib/s3mpi/format.rb
+++ lib/s3mpi/format.rb
@@ -4,11 +4,8 @@ module S3MPI
   module Format
 
     def parse_json_allowing_quirks_mode obj
-      JSON.parse(obj || 'null')
-    rescue JSON::ParserError => e
       JSON.parse(obj || 'null', quirks_mode: true)
     end
 
   end
 end
-
--- spec/s3mpi/converters/csv_spec.rb
+++ spec/s3mpi/converters/csv_spec.rb
@@ -1,24 +1,30 @@
 require 'spec_helper'
 
-module S3MPI
-  module Converters
-    describe CSV do
+describe S3MPI::Converters::CSV do
+  let(:csv_file_path) { 'spec/support/test.csv' }
+  let(:csv_data_string) { File.read csv_file_path }
 
-      let(:csv_file_path) { 'spec/support/test.csv' }
-      let(:csv_data_string) { File.read csv_file_path }
+  let(:csv_as_array_of_hashes) {
+    [
+      {'integer' => 1, 'string' => 'user1@test.com', 'float' => 1.11},
+      {'integer' => 2, 'string' => 'user2@test.com', 'float' => 2.22},
+      {'integer' => 3, 'string' => 'user3@test.com', 'float' => 3.33},
+    ]
+  }
 
-      describe '#convert_to_obj' do
-        subject { described_class.string_to_obj csv_data_string }
+  describe '#string_to_obj' do
+    subject { described_class.string_to_obj csv_data_string }
 
-        it_behaves_like 'a parsed CSV'
-      end
+    it_behaves_like 'a parsed CSV'
 
-      describe '#file_to_obj' do
-        subject { described_class.file_to_obj csv_file_path }
+    it { is_expected.to eql csv_as_array_of_hashes }
+  end
 
-        it_behaves_like 'a parsed CSV'
+  describe '#file_to_obj' do
+    subject { described_class.file_to_obj csv_file_path }
 
-      end
-    end
+    it_behaves_like 'a parsed CSV'
+
+    it { is_expected.to eql csv_as_array_of_hashes }
   end
 end
