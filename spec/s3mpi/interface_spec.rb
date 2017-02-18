966befce7ce11d56339f6cf37ffdfdbde50eec94
David Feldman <david.feldman@avant.com>
2017-02-17 18:33:15 -0600

    #converter spec

--- spec/s3mpi/interface_spec.rb
+++ spec/s3mpi/interface_spec.rb
@@ -17,15 +17,28 @@ describe S3MPI::Interface do
     it { is_expected.to eql path }
   end
 
-  describe '#exists?' do
+  describe '#converter' do
+    def expect_converter(key, klass)
+      expect(interface.converter(key)).to eql klass
+    end
 
+    it('json'){ expect_converter :json, S3MPI::Converters::JSON }
+    it('csv') { expect_converter :csv,  S3MPI::Converters::CSV  }
+    it('string'){ expect_converter :string, S3MPI::Converters::Identity }
+    it('identity'){ expect_converter :identity, S3MPI::Converters::Identity }
+
+    it "raises for unknown keys" do
+      err = S3MPI::Converters::UnknownConverterError
+      msg = ":foo is not a known converter!"
+      expect{ interface.converter(:foo) }.to raise_error(err, msg)
+    end
   end
 
   describe '#s3_object' do
 
   end
 
-  describe '#converter' do
+  describe '#exists?' do
 
   end
 
