8591a6fcf7d710bfafad6906e9dca3e9044b6003
David Feldman <david.feldman@avant.com>
2017-02-19 13:41:50 -0600

    bump version to 0.1.0.0

--- lib/s3mpi/version.rb
+++ lib/s3mpi/version.rb
@@ -1,9 +1,9 @@
 module S3MPI
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 0
-    PATCH = 8
-    BUILD = 1
+    MINOR = 1
+    PATCH = 0
+    BUILD = 0
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
   end
